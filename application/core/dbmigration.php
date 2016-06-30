38d937761ab61e6d9782b68af1783e1faeebd1d5
Monyk <monyashadow@gmail.com>
2016-06-30 23:53:20 +0300

    Migration created

---
+++ application/core/dbmigration.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Monyk
+ * Date: 30.06.2016
+ * Time: 23:02
+ */
+function migrate()
+{
+    $mysqli = new mysqli('mnotes.org', 'root', '', 'database');
+    if ($mysqli->connect_errno)
+        echo "Failed to connect to MySQL: (" . $this->mysqli->connect_errno . ") " . $this->mysqli->connect_error;
+    $mysqli->query('CREATE TABLE IF NOT EXISTS `notes` (
+          `id` INT(11) NOT NULL,
+          `text` TEXT NOT NULL,
+          `label` VARCHAR(30) NOT NULL,
+          `color` VARCHAR(20) NOT NULL,
+          `tags` TEXT NOT NULL,
+          `email` VARCHAR(30) NOT NULL
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;');
+    $mysqli->query('CREATE TABLE IF NOT EXISTS `orders` (
+          `id` INT(11) NOT NULL,
+          `email` VARCHAR(30) NOT NULL,
+          `order` TEXT
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;');
+    $mysqli->query('CREATE TABLE IF NOT EXISTS `tags` (
+          `tagId` int(11) NOT NULL,
+          `email` varchar(30) NOT NULL,
+          `tagName` varchar(30) NOT NULL
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;');
+    $mysqli->query('CREATE TABLE IF NOT EXISTS `users` (
+          `id` int(11) NOT NULL,
+          `email` varchar(30) NOT NULL,
+          `password` varchar(100) NOT NULL
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;');
+
+
+    $mysqli->query('ALTER TABLE `notes` ADD PRIMARY KEY (`id`);');
+    $mysqli->query('ALTER TABLE `orders`ADD PRIMARY KEY (`id`);');
+    $mysqli->query('ALTER TABLE `tags` ADD PRIMARY KEY (`tagId`);');
+    $mysqli->query('ALTER TABLE `users` ADD PRIMARY KEY (`id`);');
+
+    $mysqli->query('ALTER TABLE `notes`
+          MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;');
+    $mysqli->query('ALTER TABLE `orders`
+          MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;');
+    $mysqli->query('ALTER TABLE `tags`
+          MODIFY `tagId` int(11) NOT NULL AUTO_INCREMENT;');
+    $mysqli->query('ALTER TABLE `users`
+          MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;');
+}
+
+function rollback()
+{
+    $mysqli = new mysqli('mnotes.org', 'root', '', 'database');
+    if ($mysqli->connect_errno)
+        echo "Failed to connect to MySQL: (" . $this->mysqli->connect_errno . ") " . $this->mysqli->connect_error;
+    $mysqli->query('DROP TABLE IF EXISTS `notes`');
+    $mysqli->query('DROP TABLE IF EXISTS `users`');
+    $mysqli->query('DROP TABLE IF EXISTS `tags`');
+    $mysqli->query('DROP TABLE IF EXISTS `users`');
+    $mysqli->query('DROP TABLE IF EXISTS `orders`');
+}
--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 session_start();
 ini_set('display_errors',1);
+include_once 'application/core/dbmigration.php';
 include_once 'application/core/model.php';
 include_once 'application/core/view.php';
 include_once 'application/core/controller.php';
 include_once 'application/core/route.php';
+//migrate();
+//rollback();
 Route::start();
